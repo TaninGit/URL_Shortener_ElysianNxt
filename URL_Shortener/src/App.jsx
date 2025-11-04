7afac5c8c27493b18ac99ec5bd5e6b851bd41ebe
TaninGit <chieangtanin@gmail.com>
2025-11-04 23:17:05 +0700

    add all logic in web

--- URL_Shortener/src/App.jsx
+++ URL_Shortener/src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from "react"
+import CopyButton from "./components/CopyButton"
+function  App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("")
+  const [shortUrl, setShortUrl] = useState("")
+
+  const shortenUrl = async () => {
+    try {
+      const response = await fetch('https://api.ogli.sh/link', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer lnk_8163bb55cb527f4d41f24c9991d1c7ce1838a7098f5997cbd032e288730be151'
+        },  
+        body: JSON.stringify({ 
+          targetUrl: url,
+          useOriginalMeta : true
+
+        })
+      })
+      const newShortUrl = await response.json()
+      if(newShortUrl.url) {
+        setShortUrl(newShortUrl.url)
+      } else {
+        alert("Error: " + JSON.stringify(data));
+      }
+    }catch (error) {
+      console.error("Error shortening URL:", error)
+    }
+    console.log("URL to shorten:", url)
+    console.log("Shortened URL:", shortUrl)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 style={{color:"blue", border:"1px solid black"}}>Welcome to my-app</h1>
+      <input 
+        type="text" 
+        placeholder="Enter your long URL" 
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <button onClick={shortenUrl}>Shorten</button>
+      <h1>{shortUrl}</h1>
+      <CopyButton text={shortUrl} />
     </>
   )
 }
